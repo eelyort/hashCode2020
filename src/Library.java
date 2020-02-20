42875b4541cfb2d8a9a86b9fb7c9af0c925965ee
Troy Lee <troylee3006@tamu.edu>
2020-02-20 12:29:38 -0600

    Library

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,20 @@
+import java.util.*;
+import java.io.*;
+
 public class Library {
+    Set<Book> bookSet = new HashSet<Book>();
+    int numBooks;
+    int numDays;
+    int booksPerDay;
+    public Library(Book[] books, int nB, int nD, int bPD){
+        numBooks = nB;
+        numDays = nD;
+        booksPerDay = bPD;
+        for (int i = 0; i < books.length; i++) {
+            bookSet.add(books[i]);
+        }
+    }
+    public Book[] getTopN(int n){
+
+    }
 }
