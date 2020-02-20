7dc43213b8ada747fa1bf6ccceaba902d90e3727
WolvesareElite123 <WolvesareElite123@users.noreply.github.com>
2020-02-20 13:44:59 -0600

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -3,12 +3,14 @@ import java.io.*;
 
 public class Library {
     Set<Book> bookSet = new HashSet<Book>();
+    int id;
     int numBooks;
     int numDays;
     int booksPerDay;
     PriorityQueue<Book> pQueue;
-    int lastVal;
-    public Library(Book[] books, int nB, int nD, int bPD){
+    long lastVal;
+    public Library(int libID, Book[] books, int nB, int nD, int bPD){
+        id = libID;
         numBooks = nB;
         numDays = nD;
         booksPerDay = bPD;
@@ -24,19 +26,50 @@ public class Library {
 
     public void remove(Set<Book> book)
     {
-        
+        Set<Book> temp = book;
+        Iterator<Integer> itr = temp.Iterator();
+        while(itr.hasNext())
+        {
+            if(bookSet.contains(itr.next()))
+            {
+                bookSet.remove(itr.next());
+            }
+        }
+        lastVal = calTotalValue();
     }
 
     public Set<Book> score(int time)
     {
-
+        time-=numDays;
+        int num = time*booksPerDay;
+        PriorityQueue<Integer> pq = SetPQ(bookSet);
+        Set<Book> ret = new HashSet<Book>();
+        for(int x = 0; x<num; x++)
+        {
+            ret.add(pq.poll());
+        }
+        lastVal = calcTotalValue();
+        return ret;
     }
 
+////
+    public long calcTotalValue()
+    {
+        Iterator<Integer> itr = bookSet.Iterator();
+        long sum = 0;
+        while(itr.hasNext())
+        {
+            Book temp = itr.next();
+            sum+=temp.getValue();
+        }
+        return sum;
+    }
     public int calcBookValues(Set<Book> book, int num)
     {
         PriorityQueue<Integer> pq = SetPQ(book);
         Book a;
         int sum = 0;
+        num*=booksPerDay;
         for(int x = 0; x<num; x++)
         {
             a = pq.poll();
@@ -51,7 +84,7 @@ public class Library {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
         while(itr.hasNext())
         {
-            pq.add(itr);
+            pq.add(itr.next());
         }
         return pq;
     }
